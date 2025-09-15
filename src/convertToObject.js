3546ef5219307157edc88530781e513d4e190490
Roman <hpp1ovh@gmail.com>
2025-09-15 10:02:44 +0300

    solved task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,16 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString
+    .split(';')
+    .map((s) => s.trim())
+    .filter(Boolean)
+    .map((s) => s.split(':').map((item) => item.trim()))
+    .reduce((obj, [key, value]) => {
+      obj[key] = value;
+
+      return obj;
+    }, {});
 }
 
 module.exports = convertToObject;
